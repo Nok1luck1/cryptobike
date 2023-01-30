43cbafb5fc8423306baac1c00162750b77722700
Nikolaj <kolambo4ka@gmail.com>
2023-01-30 18:04:35 +0200

    dyx

--- contracts/BYKER.sol
+++
@@ -1,7 +0,0 @@
-// //SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.0;
-
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
-
-// contract BYKER is ERC721 {}
